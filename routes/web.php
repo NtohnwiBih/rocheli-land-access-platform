b3a5c2900ca30092403eac5481a03e593b477f4d
ToniBih <tonibih38@gmail.com>
2026-07-14 09:34:19 +0100

    Project B snapshot

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::inertia('/', 'site/index')->name('home');
+Route::inertia('/properties', 'site/properties')->name('properties');
+Route::inertia('/land-access-club', 'site/land-club')->name('land-club');
+Route::inertia('/services', 'site/services')->name('services');
+Route::inertia('/about', 'site/about')->name('about');
+Route::inertia('/resources', 'site/resources')->name('resources');
+Route::inertia('/contact', 'site/contact')->name('contact');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});
+
+require __DIR__.'/settings.php';
